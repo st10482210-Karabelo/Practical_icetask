0133bd051d25a0343e4e41a46adbe1cc0fc2c406
st10482210-Karabelo <st10482210@myemeris.edu.za>
2026-09-13 13:54:42 +0200

    uploaded to github

---
+++ src/main/java/com/mycompany/zedcarcompany/ZedCarCompany.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zedcarcompany;
+
+/**
+ *
+ * @author karab
+ */
+public class ZedCarCompany {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
