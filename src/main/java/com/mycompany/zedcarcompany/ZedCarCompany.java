833ed2b8c19f0a99597b8afb1ebd5cc354f362a7
st10482210-Karabelo <st10482210@myemeris.edu.za>
2026-09-13 22:25:22 +0200

    Update ZedCarCompany.java

--- src/main/java/com/mycompany/zedcarcompany/ZedCarCompany.java
+++ src/main/java/com/mycompany/zedcarcompany/ZedCarCompany.java
@@ -4,10 +4,72 @@
 
 package com.mycompany.zedcarcompany;
 
-
+import java.util.Scanner;
 public class ZedCarCompany {
 
     public static void main(String[] args) {
 
-    }
+       Scanner carchoice = new Scanner(System.in);
+        CarInventory theZedCarComp = new CarInventory();
+        
+        //display the menu
+        System.out.println("Select a vehicle car type: 1-Mercedes , 2-BMW, 3- Suzki");
+        int choiceOfUser = carchoice.nextInt();
+        
+        System.out.println("Select engine size: 1- Four wheel , 2- Six wheels");
+        int choiceofengine = carchoice.nextInt();
+        
+        System.out.println("Select the the injector type: 1- Diesel , 2- Petrol");
+        int choiceOfinjector = carchoice.nextInt();
+        
+        try{
+            CarType type;
+            switch(choiceOfUser){
+                case 1: type = CarType.Mercedes;
+                break;
+                case 2:type = CarType.BMW;
+                break;
+                
+                case 3: type = CarType.Suzki;
+                break;
+                
+                default: throw new InvalidCarTypeException("Invalid car type" + choiceOfUser);
+            }
+            
+            EngineSize theengine;
+            switch (choiceofengine){
+                case 1: engine = EngineSize.FOUR_Wheel;
+                break;
+                case 2: engine = EngineSize.Six_Wheel;
+                break;
+                
+                default: throw new InvalidCarTypeException("Invalid engine size" + theengine);
+                
+                
+               
+            }
+            
+            InjectorType injector;
+            switch (choiceOfinjector){
+                case 1: injector = InjectorType.Petrol;
+                break;
+                case 2: injector = InjectorType.Diesel;
+                break;
+                
+                default: throw new InvalidCarTypeException("Invalid type of injector: " +choiceOfinjector );
+               
+            }
+            
+            Car Found = inventory.findMatch(type,theengine , injector );
+            System.out.println(found);
+        }
+        catch(InvalidCarTypeException e){
+            System.out.println("Error:" + e.getMessage());
+        }
+        finally{
+            carchoice.close();
+        }
+        
+          
+    
 }
